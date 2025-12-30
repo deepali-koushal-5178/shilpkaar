20cfaa772cbb54098d5ce1fbb194dc755f6e6c30
deepali <deepalikoushal5178@gmail.com>
2025-12-30 11:49:41 +0530

    next.js config file change

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ["en", "hi"],
-    defaultLocale: "en",
-    localeDetection: true  
-  }
+  reactStrictMode: true,
 };
 
 export default nextConfig;
